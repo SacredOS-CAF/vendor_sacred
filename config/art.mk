5d62bb4f7b22a106c741aa0ae895660d15bfcf5d
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-05 11:46:23 +0100

    art: enable minimize debug info
    Change-Id: I12b65c532201bf31f12cfb1d62c4a8bbe2204e2f

--- config/art.mk
+++ config/art.mk
@@ -5,15 +5,16 @@ ART_BUILD_TARGET_DEBUG := false
 ART_BUILD_HOST_NDEBUG := true
 ART_BUILD_HOST_DEBUG := false
 
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
 # Default compiler
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed
 
 # Preopt apks like GMS chimara modules
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
-
-# Create profile for boot image
-# Outputs to /system/etc/boot-image.prof
-#PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 
 # Speed apps
 PRODUCT_DEXPREOPT_SPEED_APPS += \
